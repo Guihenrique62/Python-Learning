61cc654120b5e80d321b78e0ffca2918bb084af2
Guilherme Henrique <guilhermeportosantos1@gmail.com>
2023-12-22 17:25:20 -0300

    first step

---
+++ aula01.py
@@ -0,0 +1,8 @@
+print('----------------------------------')
+nome = input('Digite o nome do usuario: ')
+peso = input('Digite o seu peso: ')
+altura = input('Digite sua altura: ')
+
+imc = peso / (altura * 2)
+
+print('Seu nome é: ', nome, 'e seu IMC é', imc)
